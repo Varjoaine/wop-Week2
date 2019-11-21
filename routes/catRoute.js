c1310c43d97e5e00d9215a5482247c255cab6b3b
Hanna Hoffren <hanna.hoffren@myy.haaga-helia.fi>
2019-11-21 12:06:31 +0200

    lisatty

--- routes/catRoute.js
+++ routes/catRoute.js
@@ -1,2 +1,52 @@
 'use strict';
-// catRoute
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
+const catController = require('../controllers/catController');
+
+router.get('/', catController.cat_list_get);
+
+router.get('/:id', catController.cat_get);
+
+router.post('/',  upload.single('cat'), (req, res, next) => {
+  console.log('cat post file', req.file);
+  // tiedostonnimi bodyyn, jos haluaa
+  // req.body.filename = req.file.filename; // if you want to save filename to body
+  console.log('cat post file', req.file);
+  if (req.file === undefined) {
+    res.json({
+      error: 'No file',
+    });
+  } else if(!req.file.mimetype.includes('image')){
+    res.json({
+      error: 'Not an image',
+    });
+  } else {
+  next();
+  }
+});
+
+router.post(
+  '/', [
+    body('name', 'cannot be empty').isLength({min: 1}),
+    body('age', 'must be number').isNumeric().isLength({min: 1}),
+    body('weight', 'must be number').isNumeric().isLength({min: 1}),
+    body('owner', 'must be number').isNumeric().isLength({min: 1}),
+  ],
+  catController.cat_create_post,
+);
+
+router.put(
+  '/', [
+    body('name', 'cannot be empty').isLength({min: 1}),
+    body('age', 'must be number').isNumeric().isLength({min: 1}),
+    body('weight', 'must be number').isNumeric().isLength({min: 1}),
+    body('owner', 'must be number').isNumeric().isLength({min: 1}),
+  ],
+  catController.cat_update_put,
+);
+
+router.delete('/:id', catController.cat_delete);
+
+module.exports = router;
---
+++ routes/userRoute.js
@@ -0,0 +1,30 @@
+'use strict';
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+router.get('/', userController.user_list_get);
+
+router.get('/:id', userController.user_get);
+
+router.post(
+
+'/',
+    [
+      body('name', 'minimum 3 characters').isLength({min: 3}),
+      body('email', 'email is not valid').isEmail(),
+      body('passwd', 'at least one upper case letter').matches('(?=.*[A-Z]).{8,}'),
+      sanitizeBody('notifyOnReply').toBoolean()
+    ],
+    userController.user_create_post,
+);
+
+router.put('/', (req, res) => {
+  res.send('With this endpoint you can edit users.');
+});
+
+router.delete('/', (req, res) => {
+  res.send('With this endpoint you can delete users.');
+});
+
+module.exports = router;
